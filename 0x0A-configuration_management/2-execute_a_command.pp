272a621abd6667ef7a0db3454ea13bb84a4c65e0
Mimadfaoussi <imadfaoussi@gmail.com>
2024-06-29 06:01:26 +0200

    fixing indentation

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,7 @@
 # Using Puppet, create a manifest that kills a process named killmenow.
 
 exec { 'pkill':
-    command => 'pkill killmenow',
-    onlyif  => 'pgrep killmenow',
+    command  => 'pkill killmenow',
+    onlyif   => 'pgrep killmenow',
     provider => 'shell',
 }
